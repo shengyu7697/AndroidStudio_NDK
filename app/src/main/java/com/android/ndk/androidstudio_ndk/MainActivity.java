4e527b359e54fd88f3e85d3454a4c9dba6a179db
shengyu <shengyu7697@gmail.com>
2016-03-01 14:36:52 +0800

    init

---
+++ app/src/main/java/com/android/ndk/androidstudio_ndk/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android.ndk.androidstudio_ndk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
